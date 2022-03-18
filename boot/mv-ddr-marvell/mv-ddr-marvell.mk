31e2c6288aeaabbf836b30ef23250e51d71aa8a0
Joachim Wiberg <troglobit@gmail.com>
2022-03-18 10:47:10 +0100

    boot/mv-ddr-marvell: bump to HEAD as of 2022-01-17
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- boot/mv-ddr-marvell/mv-ddr-marvell.mk
+++ boot/mv-ddr-marvell/mv-ddr-marvell.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-# This is the latest commit on mv-ddr-devel as of 20201207
-MV_DDR_MARVELL_VERSION = 305d923e6bc4236cd3b902f6679b0aef9e5fa52d
+# This is the latest commit on mv-ddr-devel as of 2022-01-17
+MV_DDR_MARVELL_VERSION = 7753d4b3e42036d44bf3d5b68109f0c7d7aadbcc
 MV_DDR_MARVELL_SITE = $(call github,MarvellEmbeddedProcessors,mv-ddr-marvell,$(MV_DDR_MARVELL_VERSION))
 MV_DDR_MARVELL_LICENSE = GPL-2.0+ or LGPL-2.1 with freertos-exception-2.0, BSD-3-Clause, Marvell Commercial
 MV_DDR_MARVELL_LICENSE_FILES = ddr3_init.c
