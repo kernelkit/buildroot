eea524e6ec3465a1e33fae1c5fa78ce4e83a8550
Mattias Walström <lazzer@gmail.com>
2024-05-13 16:29:44 +0200

    Bump sysrepo, netopeer2, libyang and libnetconf

--- package/sysrepo/sysrepo.mk
+++ package/sysrepo/sysrepo.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SYSREPO_VERSION = 2.2.105
+SYSREPO_VERSION = 2.2.170
 SYSREPO_SITE = $(call github,sysrepo,sysrepo,v$(SYSREPO_VERSION))
 SYSREPO_INSTALL_STAGING = YES
 SYSREPO_LICENSE = BSD-3-Clause
