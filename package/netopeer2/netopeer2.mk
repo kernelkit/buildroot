475ea17ffe80507af42657b297a6f2815699b021
Mattias Walström <lazzer@gmail.com>
2024-09-16 10:27:55 +0200

    Bump netopeer2, libyang, sysrepo and libnetconf2

--- package/netopeer2/netopeer2.mk
+++ package/netopeer2/netopeer2.mk
@@ -3,7 +3,8 @@
 # netopeer2
 #
 ################################################################################
-NETOPEER2_VERSION = 2.2.28
+
+NETOPEER2_VERSION = 2.2.31
 NETOPEER2_SITE = $(call github,CESNET,Netopeer2,v$(NETOPEER2_VERSION))
 NETOPEER2_DL_SUBDIR = netopeer2
 NETOPEER2_LICENSE = BSD-3-Clause
