24f5e2c2b889c7cd16dbed7f84900e4d56e67e82
Maxime Leroy <maxime@leroys.fr>
2025-12-01 15:48:19 +0100

    package/frr: install headers on staging
    Since FRR 10.5, header files can be properly installed [1]. Enable their
    installation in Buildroot so that packages depending on FRR’s headers
    can build against them.
    
    With these headers available, the Grout package can build its zebra
    dplane plugin, which allows FRR to configure the Grout router instead of
    the kernel for packet processing.
    
    [1] https://github.com/FRRouting/frr/pull/19351
    
    Signed-off-by: Maxime Leroy <maxime@leroys.fr>
    [Julien:
     - move FRR_INSTALL_STAGING up
     - add "package/" in commit title
    ]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/frr/frr.mk
+++ package/frr/frr.mk
@@ -23,6 +23,7 @@ FRR_LICENSE_FILES = \
 FRR_CPE_ID_VENDOR = frrouting
 FRR_CPE_ID_PRODUCT = frrouting
 FRR_AUTORECONF = YES
+FRR_INSTALL_STAGING = YES
 
 FRR_DEPENDENCIES = host-frr readline json-c libyang \
 	$(if $(BR2_PACKAGE_C_ARES),c-ares) \
